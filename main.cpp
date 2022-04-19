b6c7215b5d41f82f8d39534b165c1ebb50066b94
DarshitJain04 <jain.38@iitj.ac.in>
2022-04-19 20:03:34 +0530

    added code for reading only arithmetic equations

--- main.cpp
+++ main.cpp
@@ -3,5 +3,40 @@
 using namespace std;
 
 int main(){
+
+    vector<string> ignoreStatements = {"module", "reg", "initial", "$display", "end", "endmodule"};
+
+    ifstream testFile;
+    testFile.open("test.txt");
+    if (testFile.is_open()) {
+        string str; 
+        while (getline(testFile, str))
+        {
+            bool found = false;
+            for(auto ignore : ignoreStatements){
+                if (str.find(ignore) != string::npos) {
+                    found = true;
+                    break;
+                }
+            }
+            if(!found){
+                istringstream ss(str);
+                string word;
+                int count = 0;
+                if(str.length() == 12){
+                    while (ss >> word) 
+                    {
+                        cout << word << "\n";
+                    }
+                }else{
+                    while (ss >> word) 
+                    {
+                        cout << "assignment " << word << "\n";
+                    }
+                }
+            }
+        }    
+    }
+    testFile.close();
     return 0;
 }
