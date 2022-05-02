686c8e8ec89835b6127966166b7a448a4f0909df
Aditi Goyal <56929057+gaditi123@users.noreply.github.com>
2022-05-02 21:21:43 +0530

    added comments to dfg code

--- dfg.cpp
+++ dfg.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 unordered_map<string, string> operatorsMap;
@@ -15,16 +14,22 @@ int countAND=0;
 int countOR = 0;
 int countNOT = 0;
 
+//output .dot file path
 char result[] = "C:\\Users\\HP'\\Desktop\\semester 5\\Embedded System\\embedded_project\\resultDFG.dot";
 //char result[] = "D:\\embedded_project\\resultDFG.dot";
 ofstream fout(result);
 
+//the help function is used to write the equation statements of verilog code to the Graphviz syntax style.
+//it takes in the input of the 2 operands, the operator(operatorArgument) and the answerVariable into which the result of the equation is stored
+//it has an if-else ladder for each operator called as operatorArgument
 void help(vector<string> &answerVariable, vector<string> &operands, string operatorArgument){
     if(operatorArgument == "+"){
         countPlus++;
         char plusChar = countPlus + '0';
         operatorsMap["+"][4] = plusChar;
-        fout<<operatorsMap["+"]<<endl;
+
+        //writing to the output .dot file to get the assignment statement graph
+        fout<<operatorsMap["+"]<<endl; 
         fout<<operands[0]<<" -> plus"<<plusChar<<";"<<endl;
         fout<<operands[1]<<" -> plus"<<plusChar<<";"<<endl;
         fout << answerVariable[0] << " [fillcolor = lavender, style = filled];\n";
@@ -33,6 +38,8 @@ void help(vector<string> &answerVariable, vector<string> &operands, string opera
         countMinus++;
         char minusChar = countMinus + '0';
         operatorsMap["-"][5] = minusChar;
+
+        //writing to the output .dot file to get the assignment statement graph
         fout<<operatorsMap["-"]<<endl;
         fout<<operands[0]<<" -> minus"<<minusChar<<";"<<endl;
         fout<<operands[1]<<" -> minus"<<minusChar<<";"<<endl;
@@ -118,8 +125,10 @@ void help(vector<string> &answerVariable, vector<string> &operands, string opera
 
 int main(){
 
+    //the following statements are ignored/not processed when reading the input verilog file since thet are of no use in making the DFG
     vector<string> ignoreStatements = {"module", "reg", "initial", "$display", "end", "endmodule", "wire", "input", "output"};
 
+    //the different assignment and logical operators are being matched to ther UNICODE values to be wriiten to in Graphviz syntax. The color coding is done for all the operators and = sign to help in better visulisation
     operatorsMap.insert({"=", "equali [label = <&#061;> ,fillcolor=greenyellow, style=filled]"});
     operatorsMap.insert({"+", "plusi [label = <&#043;> ,fillcolor=lightblue, style=filled]"});
     operatorsMap.insert({"-", "minusi [label = <&#045;> ,fillcolor=lightblue, style=filled]"});
@@ -134,48 +143,63 @@ int main(){
 
     fout << "digraph result {" << endl;
     
+    //defining input verilog .txt file and storing it in an ifstream variable
     ifstream testFile;
     testFile.open("dfgTest4.txt");
+
+    //defining the counter varianles for identifing various parts of the verilog code lines
     vector<string> variableNames;
     vector<int> variableValues;
     vector<string> answerVariable;
     vector<string> operands;
     vector<string> operators;
 
-    if (testFile.is_open()) {
+
+    if (testFile.is_open()) { //checking if the input file is found and if its opened
         string str; 
         while (getline(testFile, str))
         {
             bool found = false;
+            //while parsing the input verilog file if the str retrived has any of the ignoreStatements then control shifts to the next line
             for(auto ignore : ignoreStatements){
                 if (str.find(ignore) != string::npos) {
                     found = true;
                     break;
                 }
             }
-            if(!found){
+            if(!found){ //this block executes if the current line does not contain any keywords which are part of ignoreStatements
                 string word;
                 istringstream ss(str);
 
-                int variableDetectionCounter = 0;
-                int equationDetectionCounter = 0;
+                int variableDetectionCounter = 0; //used to count the space separated word in assignment statemets
+                int equationDetectionCounter = 0; //used to count the space separated word in equation statemets
 
-                if(str.length() == 11){
+                //The assignment statements such as datax = 11 ; are of length 11 and its determination of various parts are done in this if block 
+                if(str.length() == 11){ 
                     variableDetectionCounter = 0;
-                    countEqual++;
-                    char equalChar = countEqual + '0';
-                    operatorsMap["="][5] = equalChar;
-                    fout<<operatorsMap["="]<<endl;
+                    countEqual++; //counting the number of assignment statements as those many = operatos we will have to output in the Graphviz .dot file
+                    char equalChar = countEqual + '0'; //converting number of equal operatos from int to character
+                    operatorsMap["="][5] = equalChar; //getting UNICODE vale of = opertor usning the map defined above
+                    
+                    //fout is used to write the Graphiv syntax to the external .dot file for visulising the DFG
+                    fout<<operatorsMap["="]<<endl; //adding the code to output an = operator into the graph
+
+                    //the while loop separetes a string into a list of space separated words and iterates over it
                     while (ss >> word) 
                     {
+                        //variableDetectionCounter keeps track of different components of assignment statements
+                       //variableDetectionCounter = 0 is to detect the variable name into which the value is getting stored. 
                        if(variableDetectionCounter == 0) 
                        {    
                            variableNames.push_back(word);
                            fout << word << " [fillcolor = lightcoral, style=filled]\n";
                            fout<<"equal"<<equalChar<<" -> "<< word <<";"<<endl;
                        }
+                       //variableDetectionCounter = 1 is for the = operator. It is not necessary for us to record this since all assignment operators will have an equal symbol and no new information is gathered.
+
+                       //variableDetectionCounter =2 is to detect the value which is assigned to the variable
                        else if(variableDetectionCounter == 2) {
-                           word = word.substr(0, word.size()-1);
+                           word = word.substr(0, word.size() - 1); // to remove the semicolon in the end
                            variableValues.push_back(stoi(word));
                            fout << word << " [fillcolor = khaki, style=filled]\n";
                            fout<<word<<" -> "<< "equal" << equalChar<< ";"<<endl;
@@ -183,23 +207,34 @@ int main(){
                        variableDetectionCounter++;
                     }
                 }else{
+                     //equationDetectionCounter keeps track of different components of equation statements
                     equationDetectionCounter = 0;
                     string operatorArgument;
+
+                    //the while loop separetes a string into a list of space separated words and iterates over it
                     while (ss >> word) 
                     {
+                        //equationDetectionCounter = 0 is used to detect the answer variable storing the result of the operation performed
                         if(equationDetectionCounter == 0){
                             answerVariable.push_back(word);
-                        }else if(equationDetectionCounter == 2 or equationDetectionCounter == 4){
+                        }
+                        //equationDetectionCounter = 2 or 4 detecs the 2 operands present in the equation
+                        else if(equationDetectionCounter == 2 or equationDetectionCounter == 4){
                             if(equationDetectionCounter == 4){
                                 word = word.substr(0, word.size()-1);
                             }
                             operands.push_back(word);
-                        }else if(equationDetectionCounter == 3){
+                        }
+                        //equationDetectionCounter = 3 detects the operand being used on the 2 operators
+                        else if(equationDetectionCounter == 3){
                             operatorArgument = word;
                         }
                         equationDetectionCounter++;
                     }
+                    //the help() fucntion is called to firstly convert the operator symbol to its UNOCODE representation and also form the nodes and edges representation of the equation statements 
                     help(answerVariable, operands, operatorArgument);
+
+                    //clear the answerVariable and operands list 
                     answerVariable.clear();
                     operands.clear();
                 }
@@ -208,7 +243,9 @@ int main(){
         }    
     }
 
+    //marking the end of the output file to which the graph visulation code in Graphviz syntax is being written
     fout<<"}"<<endl;
+    //closing the file from which the verilog code input is being read
     testFile.close();
 
     return 0;
